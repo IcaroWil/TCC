64f878522c14a550c96ed6c913c3ad25dd896104
IcaroCX <icaro.oliveira@xtentgroup.com>
2025-08-09 09:35:28 -0300

    Add environment configuration, database connection setup, and JWT authentication service implementation

---
+++ src/infrastructure/external-services/JWTAuthService.ts
@@ -0,0 +1,37 @@
+import { IAuthService } from "../../domain/services/IAuthService";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export class JWTAuthService implements IAuthService {
+    private readonly jwtSecret: string;
+    private readonly jwtExpiration: string;
+
+    constructor() {
+        this.jwtSecret = process.env.JWT_SCRET || 'Minha chave';
+        this.jwtExpiration = process.env.JWT_EXPIRATION || '24';
+    }
+
+    async generateToken(userId: string): Promise<string> {
+        return jwt.sign({ userId }, this.jwtSecret, {
+            epxpiresIn : this.jwtExpiration,
+        });
+    }
+
+    async verifyToken(token: string): Promise<{ userId: string } | null {
+        try {
+            const decoded = jwt.verify(token, this.jwtSecret) as { userId: string };
+            return decoded;
+        } catch {
+            return null;
+        }
+    }
+
+    async hashPassword(password: string): Promise<string> {
+        const saltRounds = 10;
+        return bcrypt.hash(password, saltRounds);
+    }
+
+    async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+        return bcrypt.compare(password, hashedPassword)
+    }
+}
