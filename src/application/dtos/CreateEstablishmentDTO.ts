8548f2dc1c55e9704151815616de2e65c742a81e
IcaroCX <icaro.oliveira@xtentgroup.com>
2025-08-21 12:15:17 -0300

    Update CreateEstablishmentDTO to include new fields and types
    - Added businessCategoryId, subcategory, businessType, document, description, website, and socialMedia fields to the CreateEstablishmentDTO interface.
    - Imported BusinessType and SocialMediaLinks for better type management.

--- src/application/dtos/CreateEstablishmentDTO.ts
+++ src/application/dtos/CreateEstablishmentDTO.ts
@@ -1,3 +1,6 @@
+import { BusinessType } from '../../domain/entities/BusinessCategory';
+import { SocialMediaLinks } from '../../domain/value-objects/SocialMediaLinks';
+
 export interface CreateEstablishmentDTO {
     name: string;
     email: string;
@@ -6,5 +9,11 @@ export interface CreateEstablishmentDTO {
     city: string;
     state: string;
     zipCode: string;
-    cnpj: string;
+    businessCategoryId: string;
+    subcategory: string;
+    businessType: BusinessType;
+    document: string;
+    description?: string;
+    website?: string;
+    socialMedia?: SocialMediaLinks;
 }
